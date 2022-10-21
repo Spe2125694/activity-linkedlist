9be018381f4bdea5e946432dc7a59811c26746e1
Spe2125694 <spe2125694@maricopa.edu>
2022-10-20 19:33:11 -0700

    Updates: 10/20

--- src/main/java/org/example/linkedlist/SingleDriver.java
+++ src/main/java/org/example/linkedlist/SingleDriver.java
@@ -4,6 +4,30 @@ public class SingleDriver {
 
     public static void main(String[] args) {
 
-        System.out.println();
+        LinkedList<String> linkedList = new SinglyLinkedList<>();
+
+        linkedList.addFirst("First");
+        linkedList.addFirst("Second");
+        linkedList.addFirst("Third");
+        linkedList.addFirst("Fourth");
+        linkedList.addFirst("Fifth");
+
+        System.out.println(linkedList);
+
+        linkedList.addLast("Sixth");
+        linkedList.addLast("Seventh");
+        linkedList.addLast("Eighth");
+
+        System.out.println(linkedList);
+
+        linkedList.pollFirst();
+        linkedList.pollFirst();
+
+        System.out.println(linkedList);
+
+        linkedList.pollLast();
+        linkedList.pollLast();
+
+        System.out.println(linkedList);
     }
 }
--- src/main/java/org/example/linkedlist/SinglyLinkedList.java
+++ src/main/java/org/example/linkedlist/SinglyLinkedList.java
@@ -1,6 +1,7 @@
 package org.example.linkedlist;
 
 import java.util.ArrayList;
+import java.util.StringJoiner;
 
 public class SinglyLinkedList<E> implements LinkedList<E> {
 
@@ -10,21 +11,76 @@ public class SinglyLinkedList<E> implements LinkedList<E> {
     @Override
     public void addFirst(E element) {
 
+        Node node = new Node(element, head); // Step 1 & 2
+
+        if (head == null) {
+            head = node; // Step 3
+            tail = head;
+        } else {
+            head = node; // Step 3
+        }
     }
 
     @Override
     public void addLast(E element) {
 
+        Node node = new Node(element, null); // Step 1
+
+        if (head == null) {
+            tail = node; // Step 3
+            head = tail;
+        } else {
+            tail.next = node; // Step 2
+            tail = node; // Step 3
+        }
+
     }
 
     @Override
     public E pollFirst() {
-        return null;
+
+        E element;
+
+        if (head == null) {
+            element = null;
+        } else {
+            element = head.element; // Step 1
+
+            Node next = head.next; // Step 2
+            head.next = null;
+            head = next;
+        }
+
+        return element;
     }
 
     @Override
     public E pollLast() {
-        return null;
+
+        E element;
+        if (tail == null){ // no elements
+            element = null;
+
+        } else {
+
+            element = tail.element; // Step 1
+
+            if (head == tail) { // only 1 element
+                head = null;
+                tail = null;
+            } else {
+
+                Node current = head;
+                Node previous = head;
+                while(current.next != null){ // Step 2
+                    previous = current;
+                    current = current.next;
+                }
+                tail = previous; // Step 3
+                tail.next = null; // Step 4
+            }
+        }
+        return element;
     }
 
     @Override
@@ -45,6 +101,23 @@ public class SinglyLinkedList<E> implements LinkedList<E> {
     @Override
     public boolean contains(E element) {
         return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder("[");
+        StringJoiner joiner = new StringJoiner(", ");
+        Node current = head;
+
+        while (current != null) {
+            joiner.add(current.element.toString());
+            current = current.next;
+        }
+
+        builder.append(joiner);
+        builder.append("] ");
+
+        return builder.toString();
     }
 
     private class Node{
